9cfbe575bcde03d40a67b4057a8927f724e9accc
= <=>
2025-07-26 12:09:46 +0530

    optimize Modal image

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,15 +2,18 @@ import { useEffect } from "react";
 import cakeImage from "../assets/cake_cup.png";
 
 export default function Modal() {
-  useEffect(() => {
-    const img = new Image();
-    img.src = cakeImage;
-  }, []);
+  // useEffect(() => {
+  //   const img = new Image();
+  //   img.src = cakeImage;
+  // }, []);
 
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
       <div className="bg-white p-6 rounded">
-        <img src={cakeImage} alt="cake" className="block mx-auto mb-4" />
+        {/* <img src={cakeImage} alt="cake" className="block mx-auto mb-4" /> */}
+        <div className="w-48 h-48 mx-auto mb-4 flex items-center justify-center">
+          <img src={cakeImage} alt="cake" className="max-w-full max-h-full" />
+        </div>
         <h2 className="text-2xl font-bold text-rose-950 mb-4">
           Order Confirmed
         </h2>
